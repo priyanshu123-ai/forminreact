fb6c27db64b93bb9a46535f96cd11e1c3a28c6ca
priyanshu123-ai <priyanshukumarpriyanshu015@gmail.com>
2025-03-11 19:01:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Form from './reactform/Form'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <section>
+    <Form></Form>
+    </section>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/reactform/Form.jsx
@@ -0,0 +1,65 @@
+// /* eslint-disable no-undef */
+import { useState } from 'react';
+
+import './Form.css'
+
+function Form(){
+    const[user,setUser] = useState({
+        firstName:"",
+        lastName:"",
+        email:"",
+        password:"",
+        
+
+
+    });
+
+    const handleInputChange = (e) =>{
+        const {name,value} = e.target;
+        setUser((prev) => ({...prev,[name] : value}))
+
+    }
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+
+        const { firstName, lastName, email, password } = user; 
+        const formData = { firstName, lastName, email, password };
+        console.log(formData);
+    }
+    return(
+        <>
+    <div className="container">
+        <div className="form-content">
+        <h1>React Form</h1>
+
+<form className='form-container' onSubmit={handleFormSubmit}>
+    <div className="input-field">
+        <input type="text" name='firstName' value={user.firstName} placeholder='Enter First Name : ' onChange = {handleInputChange}/>
+    </div>
+
+    <div className="input-field">
+        <input type="text" name='Last Name' value={user.lastNameastName} placeholder='Enter last Name : ' onChange = {handleInputChange}/>
+    </div>
+
+    <div className="input-field">
+        <input type="email" name='email' value={user.email} placeholder='Enter Email : ' onChange = {handleInputChange} />
+    </div>
+
+    <div className="input-field">
+        <input type="password" name='password' value={user.password} placeholder='Enter Password : ' onChange = {handleInputChange} />
+    </div>
+
+    <button type='submit'>Submit</button>
+    
+</form>
+
+        </div>
+        
+    </div>
+
+        </>
+    )
+}
+
+export default Form;
